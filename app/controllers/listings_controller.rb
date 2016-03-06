b4e46857821061a4378f6642facc679eb8e966c7
hnauris <nauris.horsts@gmail.com>
2016-03-06 18:42:41 +0200

    Finishing 04 - 002

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,26 @@
 class ListingsController < ApplicationController
   def new
-
+    @listing = Listing.new
   end
+
+  def create
+    @listing = Listing.new(listing_params)
+
+    if @listing.save
+      redirect_to root_path
+    else
+      render new_listing_path
+    end
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  private
+
+    def listing_params
+      #allows the following params be made in the listing table
+      params.require(:listing).permit(:title, :description, :city, :state, :zipcode)
+    end
 end
